036395257acfbcd1acc88565e7c3340325b2c3b1
Trebor Rude <treborrude@gmail.com>
2012-04-03 02:34:07 +0100

    Initial import. Only "android create project" created files for now.

---
+++ src/org/rudeapps/PrayerMinder/PrayerMinder.java
@@ -0,0 +1,15 @@
+package org.rudeapps.PrayerMinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PrayerMinder extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
