0d4bd4a6093f68b2a606b38e13c1167423ed847b
Trebor Rude <treborrude@googlemail.com>
2013-03-24 09:11:00 -0600

    Rename layout for PrayerList to make it clear what it's for.

--- src/rude/trebor/apps/PrayerMinder/PrayerList.java
+++ src/rude/trebor/apps/PrayerMinder/PrayerList.java
@@ -11,6 +11,6 @@ public class PrayerList extends FragmentActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.PrayerList);
     }
 }
