ffa71ecce8edf4e42c0b0e842f3761401081355b
Trebor Rude <treborrude@googlemail.com>
2013-03-30 20:48:21 -0600

    Very beginning implementation for PrayerListFragment.

--- src/com/github/treborrude/prayerminder/PrayerListFragment.java
+++ src/com/github/treborrude/prayerminder/PrayerListFragment.java
@@ -1,5 +1,19 @@
 package com.github.treborrude.prayerminder;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+
 class PrayerListFragment extends SherlockListFragment
 {
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container, 
+                           Bundle savedInstanceState) 
+  {
+    return inflater.inflate(R.layout.prayerlist, container, false);
+  }
 }
